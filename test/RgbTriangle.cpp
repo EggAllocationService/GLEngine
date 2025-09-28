bed1e08621d7b5b49df73eefdd0039b9b42acf0d
Kyle Smith <kyle@cabotmc.dev>
2025-09-28 01:44:13 -0300

    update demo

--- test/RgbTriangle.cpp
+++ test/RgbTriangle.cpp
@@ -9,12 +9,12 @@ RgbTriangle::RgbTriangle()
 
 void RgbTriangle::Update(double DeltaTime)
 {
-	hue += DeltaTime * 360;
+	hue += DeltaTime * 120;
 	if (hue > 360) {
 		hue = 0;
 	}
 
-	Rotation += (3.14159 / 2.0) * DeltaTime;
+	Rotation += (3.14159 / 2.0) * (DeltaTime / 5);
 
 	Scale.xy = abs(sin(Rotation)) + 0.1;
 
@@ -24,20 +24,24 @@ void RgbTriangle::Update(double DeltaTime)
 
 void RgbTriangle::Draw(MatrixStack2D& stack)
 {
-	
-
 	std::vector<float3> verticies = {
 		float3(-0.866, -0.5, 1.0),
 		float3(0.866, -0.5, 1.0),
 		float3(0.0, 1.0, 1.0)
 	};
 
-	glColor4fv(Colors::hsv(360 - hue, 1.0, 1.0));
+	glColor4fv(Colors::WHITE);
 	stack.DrawRect(float2(-1.0, -1.0), float2(1.0, 1.0));
 
-	glColor4fv(Colors::hsv(hue, 1.0, 1.0));
-	stack.DrawPolygon(verticies);
+	glBegin(GL_POLYGON);
+	int i = 0;
+	for (float3& vertex : verticies) {
+		glColor4fv(Colors::hsv(((int)hue + (120 * i)) % 360, 1.0, 1.0));
+		glVertex3fv(stack * vertex);
 
+		i++;
+	}
+	glEnd();
 
 	glFlush();
 }
