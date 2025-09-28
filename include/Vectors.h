4cf46229f931f38c35e65c92f249e347d2456a4f
Kyle Smith <kyle@cabotmc.dev>
2025-09-28 18:23:58 -0300

    add more helper operators to Vectors.h

--- include/Vectors.h
+++ include/Vectors.h
@@ -14,29 +14,73 @@
 /// union {
 ///     int data[3]; // the actual storage
 ///
-///     // setup parameters:
+///     // Setup parameters:
 ///     // `vec2<int>` indicates this swizzled representation can be converted to a vec2<int>
 ///     // `int` indicates the primitive type stored is an int, allowing for broadcast assignments (foo.xy = 3)
 ///     // `3` sets the source vector width to 3 (so it fits in the union correctly)
-///     // `0, 1` indicates there are two elements in the swizzled vector, from source lane 0 and 1 respectively.
-///     vec_swizzle<vec2<int>, int, 3, 0, 1> xy;
+///     // `1, 0` indicates there are two elements in the swizzled vector, from source lane 1 and 0 respectively.
+///     // Therefore, `yx` will be a reversed view of the first two lanes of the vector.
+///     vec_swizzle<vec2<int>, int, 3, 0, 1> yx;
 ///
-///     // we can have a reversed version of the above by reversing the order of `indices`:
-///     vec_swizzle<vec2<int>, int, 3, 1, 0> yx;
+///     // We can also use `vec_swizzle` to easily allow narrowing a vector.
+///     // Since `indicies` is in ascending order here, `xy` will simply be the first two lanes of the source vector.
+///     vec_swizzle<vec2<int>, int, 3, 0, 1> xy;
 /// }
 /// @endcode
 template<typename vector, typename primitive, int len, int... indices>
 struct vec_swizzle {
     primitive data[len];
 
+    /********
+    * Vector assignment operations
+    *********/
     vector operator=(vector rhs) {
-        return vector((data[indices] = rhs[indices])...);
+        int i = 0;
+        ((data[indices] = rhs[i++]), ...);
+        return vector(data[indices]...);
     }
 
+    vector operator +=(vector rhs) {
+        int i = 0;
+        ((data[indices] += rhs[i++]), ...);
+        return vector(data[indices]...);
+    }
+
+    vector operator -=(vector rhs) {
+        int i = 0;
+        ((data[indices] -= rhs[i++]), ...);
+        return vector(data[indices]...);
+    }
+
+    vector operator *=(vector rhs) {
+        int i = 0;
+        ((data[indices] *= rhs[i++]), ...);
+        return vector(data[indices]...);
+    }
+
+    /********
+    * Scalar assignment operations
+    *********/
     vector operator=(primitive rhs) {
         return vector((data[indices] = rhs)...);
     }
 
+    vector operator+=(primitive rhs) {
+        return vector((data[indices] += rhs)...);
+    }
+
+    vector operator-=(primitive rhs) {
+        return vector((data[indices] -= rhs)...);
+    }
+
+    vector operator*=(primitive rhs) {
+        return vector((data[indices] *= rhs)...);
+    }
+
+
+    /// <summary>
+    /// Converts this swizzled view to a vector
+    /// </summary>
     operator vector() {
         return vector(data[indices]...);
     }
@@ -44,7 +88,7 @@ struct vec_swizzle {
 
 /// Constant-length vector, templated.
 ///
-/// Useful for using in other templated code, for e.g. templated matrix multiplication
+/// Useful for math in other templated code, e.g. matrix multiplication
 /// @tparam T primitive type of vector
 /// @tparam LEN vector width
 template<typename T, int LEN>
@@ -204,6 +248,20 @@ struct vec4 {
         data[3] = w;
     }
 
+    vec4(vec2<T> xy, T z, T w) {
+        data[0] = xy.x;
+        data[1] = xy.y;
+        data[2] = z;
+        data[3] = w;
+    }
+
+    vec4(vec3<T> xyz, T w) {
+        data[0] = xyz.x;
+        data[1] = xyz.y;
+        data[2] = xyz.z;
+        data[3] = w;
+    }
+
     union {
         T data[4];
 
@@ -249,7 +307,7 @@ struct vec4 {
     }
 };
 
-// basic vector types for convenience
+// define some basic vector types for convenience
 typedef vec4<float> float4;
 typedef vec4<int> int4;
 
