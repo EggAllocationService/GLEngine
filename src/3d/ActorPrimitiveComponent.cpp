2321b9bbf90ae6abe971012c89fe47f1e62262ac
Kyle Smith <kyle@cabotmc.dev>
2025-10-18 17:13:39 -0300

    Optimize ActorPrimitiveComponent.cpp

--- src/3d/ActorPrimitiveComponent.cpp
+++ src/3d/ActorPrimitiveComponent.cpp
@@ -10,10 +10,8 @@ float3 glengine::world::ActorPrimitiveComponent::GetAbsolutePosition() const {
     auto parentMatrix = GetActor()->GetTransformMatrix();
     auto resultMatrix = parentMatrix * selfMatrix;
 
-    auto testVec = vecn<float, 4>::zero();
-    testVec[3] = 1.0f;
+    // 4th column of the resulting matrix represents the origin
+    auto resultVec = *resultMatrix[3];
 
-    auto resultVec = resultMatrix * testVec;
-
-    return float3(resultVec[0], resultVec[1], resultVec[2]);
+    return {resultVec[0], resultVec[1], resultVec[2]};
 }
