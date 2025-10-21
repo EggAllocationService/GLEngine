dff4a4ab4dc2cc6fb75a74103ed6bbd82753d799
Kyle Smith <kyle@cabotmc.dev>
2025-10-21 17:50:19 -0300

    Fix mesh rendering

--- src/3d/mesh/StaticMesh.cpp
+++ src/3d/mesh/StaticMesh.cpp
@@ -9,7 +9,7 @@
 using namespace glengine::world::mesh;
 
 std::unique_ptr<StaticMesh> StaticMesh::FromOBJ(std::ifstream& file) {
-    auto result = std::make_unique<StaticMesh>();
+    auto result = new StaticMesh();
 
     int vPos = 0, vtPos = 0, vnPos = 0;
 
@@ -57,21 +57,21 @@ std::unique_ptr<StaticMesh> StaticMesh::FromOBJ(std::ifstream& file) {
                 sscanf(line.c_str(), "f %d/%d/%d %d/%d/%d %d/%d/%d", &p1, &t1, &n1,
                                                                      &p2, &t2, &n2,
                                                                      &p3, &t3, &n3);
-                result->faces_.push_back(int3(p1, t1, n1));
-                result->faces_.push_back(int3(p2, t2, n2));
-                result->faces_.push_back(int3(p3, t3, n3));
+                result->faces_.push_back(int3(p1 - 1, t1 - 1, n1 - 1));
+                result->faces_.push_back(int3(p2 - 1, t2 - 1, n2 - 1));
+                result->faces_.push_back(int3(p3 - 1, t3 - 1, n3 - 1));
             } else {
                 // this OBJ file just specifies position
                 int p1, p2, p3;
 
                 sscanf(line.c_str(), "f %d %d %d", &p1, &p2, &p3);
 
-                result->faces_.push_back(int3(p1, p2, p3));
+                result->faces_.push_back(int3(p1 - 1, p2 - 1, p3 - 1));
             }
         }
     }
 
-    return result;
+    return std::unique_ptr<StaticMesh>(result);
 }
 
 void StaticMesh::Render() {
@@ -80,9 +80,13 @@ void StaticMesh::Render() {
     float max = 0;
 
     if (hasTexCoords_ || hasNormals_) {
-        max = faces_.size() / 3.0;
+        max = faces_.size();
         // faces array is one vertex per index, with position index, texcood index, and normal index in each int3
         for (auto vertex : faces_) {
+            float color = current / max;
+            color *= 0.5;
+            color += 0.5;
+            glColor3f(color, color, color);
             if (hasTexCoords_) {
                 glTexCoord2fv(vertices_[vertex.y].texCoord);
             }
@@ -90,9 +94,10 @@ void StaticMesh::Render() {
                 glNormal3fv(vertices_[vertex.z].normal);
             }
             glVertex3fv(vertices_[vertex.x].position);
+            current++;
         }
     } else {
-        max = faces_.size();
+        max = faces_.size() / 3.0;
         // faces array is one face per index, p1, p2, p3
         for (auto face : faces_) {
             float color = current / max;
--- src/Engine.cpp
+++ src/Engine.cpp
@@ -277,7 +277,7 @@ namespace glengine {
         }
 
         glEnable(GL_DEPTH_TEST);
-        glEnable(GL_CULL_FACE);
+       // glEnable(GL_CULL_FACE);
         glCullFace(GL_BACK);
 
         glMatrixMode(GL_MODELVIEW);
