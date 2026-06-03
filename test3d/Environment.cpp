f459f85b0c51e621198c4472328c0d804e558858
Kyle Smith <kyle@cabotmc.dev>
2026-06-03 12:54:24 -0300

    fixup! add basic lighting

--- test3d/Environment.cpp
+++ test3d/Environment.cpp
@@ -15,14 +15,6 @@ Environment::Environment() {
     sun->GetTransform()->SetRotation({-PI / 4, PI / 4, 0});
     sun->Ambient = float4(0.1, 0.1, 0.1, 1);
     sun->Diffuse = float4(1, 1, 1, 1);
-
-    auto mesh = GetEngine()->GetResourceManager()->GetResource<glengine::world::mesh::StaticMesh>("enterprise.obj");
-
-    auto model = CreateComponent<glengine::world::mesh::StaticMeshComponent>();
-    model->SetMesh(mesh);
-    model->GetTransform()->SetRotation({PI / 4, PI / 4, 0});
-    model->GetTransform()->SetScale({0.5, 0.5, 0.5});
-
 }
 
 void Environment::Update(double deltaTime) {
